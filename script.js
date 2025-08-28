0c2e60aab98be9e7828a0caba0c98ab683300607
Obi815 <80441506+Obi815@users.noreply.github.com>
2025-08-27 22:08:31 -0700

    more kyu

--- script.js
+++ script.js
@@ -65,6 +65,45 @@
 ////////////////////////////// COMPLETED ///////////////////////
 
 
-// /////// copmpleted just for github
+// You are given two sorted arrays that contain only integers. These arrays may be sorted in either ascending or descending order.
+//  Your task is to merge them into a single array, ensuring that:
+// The resulting array is sorted in ascending order.
+// Any duplicate values are removed, so each integer appears only once.
+// If both input arrays are empty, return an empty array.
+// No input validation is needed, as both arrays are guaranteed to contain zero or more integers.
 
-document.querySelector('h1')
+// Examples (input -> output)
+// * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+
+// function mergerSortedArrays(arr1, arr2) {
+//     const combined = arr1.concat(arr2);
+
+//     const uniqueSet = new Set(combined);
+
+//     return Array.from(uniqueSet).sort((a, b) => a - b);
+// }
+
+// console.log(mergerSortedArrays)
+
+// /////// COMPLETED ///////
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+
+// Example(Input --> Output):
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+
+function reverse(sentence) {
+    return sentence.split('').map(function (word) {
+        return word.split('').reverse().join('');
+    }).join('');
+}
+
+console.log(reverse('!dlrow olleh'))
