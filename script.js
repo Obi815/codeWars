612d75c0213b852079b2ffd19bb775550d0d6bda
Obi815 <80441506+Obi815@users.noreply.github.com>
2025-09-19 15:16:55 -0700

    Find Find the first non-consecutive number

--- script.js
+++ script.js
@@ -315,15 +315,42 @@
 // console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']))
 
 
-function countSheeps(sheepArray) {
-    // TODO
-    return sheepArray.filter(sheep => sheep === true).length
+// function countSheeps(sheepArray) {
+//     // TODO
+//     return sheepArray.filter(sheep => sheep === true).length
 
+// }
+
+// console.log(countSheeps([true, true, true, false,
+//     true, true, true, true,
+//     true, false, true, false,
+//     true, false, false, true,
+//     true, true, true, true,
+//     false, false, true, true]))
+
+
+// completed //
+
+// Your task is to find the first element of an array that is not consecutive.
+
+// By not consecutive we mean not exactly 1 larger than the previous element of the array.
+
+//     E.g.If we have an array[1, 2, 3, 4, 6, 7, 8] 
+//     then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+
+// If the whole array is consecutive then return null2.
+
+// The array will always have at least 2 elements1 and all elements will be numbers.
+// The numbers will also all be unique and in ascending order.
+// The numbers could be positive or negative and the first non - consecutive could be either too!
+
+function firstNonConsecutive(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] !== arr[i - 1] + 1) {
+            return arr[i]
+        }
+    }
+    return null
 }
 
-console.log(countSheeps([true, true, true, false,
-    true, true, true, true,
-    true, false, true, false,
-    true, false, false, true,
-    true, true, true, true,
-    false, false, true, true]))
+console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]))
