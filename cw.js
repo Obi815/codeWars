89145b1134a3a89304e03ccd23820f43aff85877
Obi815 <80441506+Obi815@users.noreply.github.com>
2025-09-24 21:00:23 -0700

    Object work

--- cw.js
+++ cw.js
@@ -60,23 +60,5 @@
 
 // console.log(digitize([2345960]))
 
-//Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
-function StreetFighter(fightJump, fightCrouch, fightKick, fightPunch) {
-    this.jump = fightJump
-    this.crouch = fightCrouch
-    this.kick = fightKick
-    this.punch = fightPunch
-    this.slogan = function () {
-        console.log(`You can't handle my ${this.punch}`)
-    }
-    this.wind = function () {
-        alert('Haha')
-    }
 
-    this.dash = function () {
-        console.log('Whoop Missed Me!')
-    }
-}
-
-let ryu = new StreetFighter('high', 'low', 'body', 'haddduuuken')
 
---
+++ object.js
@@ -0,0 +1,51 @@
+/////////////////////////////       Object Literal ///////////////
+// const circle = { // <---- Object   : This is Object literal 
+//     radius: 1, // <--- properties           
+//     location: { // <--- properties                      
+//         x: 1,
+//         y: 1
+//     },
+//     draw: function () { // <--- Method
+//         console.log('draw');
+//     }
+// };
+
+// circle.draw();
+
+
+//                              Factory Function 
+// function createCircle(radius) {
+//     return { // <---- Object   : This is Object literal 
+//         radius, // <--- properties           
+//         draw: function () { // <--- Method
+//             console.log('draw');
+//         }
+//     };
+// }
+
+// const circle = createCircle(1);
+// circle.draw()
+
+
+//                      Constructor Function 
+
+function Circle(radius) {  // Circle with capital "C" to let us know it is a Constructor Function
+    this.radius = radius
+    this.draw = function () {
+        console.log('draw')
+    }
+}
+
+// Circle.call({}, 1); //Different way to call the function
+// Circle.apply({}, [1, 2, 3]); // if you wanted to add an array as a parameter
+
+
+// const another = new Circle(1);
+// // 'new' operator creates and empty object
+// // then will set 'this' to point to that object
+
+
+let x = { value: 10 };
+let y = x;
+
+x.value = 20
