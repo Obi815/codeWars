e9d93ce786866ee6965f81e963ef50a1a554d97b
Obi815 <80441506+Obi815@users.noreply.github.com>
2025-12-03 18:20:05 -0800

    do you speak english

--- cw.js
+++ cw.js
@@ -809,3 +809,23 @@
 // z.push(x * i) adds the current multiple of x to the z array
 // return z returns the final array of multiples
 ////////////////////////      COMPLETED //////////////////////////
+
+
+
+
+
+
+
+
+////////////////////////                NUMBER 25 ////////////////////
+// Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+// The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+// Return value as boolean values, true for the string to contains "English", false for it does not.
+
+// function spEng(sentence){
+//     return sentence.toLowerCase().includes('english')
+//   }
