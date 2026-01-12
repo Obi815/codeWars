d5ef206b89cfbd12d361b5091346181073301206
Obinna Ezeka <obinnae815@gmail.com>
2026-01-12 15:40:29 -0800

    changes

--- cw.js
+++ cw.js
@@ -901,11 +901,11 @@
 // Complete the function that takes a noun as a string, 
 // and returns her preferred band name written as a string.
 
-// function bandNameGenerator(str) {
-//   if(str[0] != str.slice(-1)){
-//     return 'The' + ' ' + str[0].toUpperCase() + str.slice(1)
-//   }
-//   else{
-//     return str[0].toUpperCase() + str.slice(1).repeat(2)
-//   }
-// }
+function bandNameGenerator(str) {
+  if(str[0] != str.slice(-1)){
+    return 'The' + ' ' + str[0].toUpperCase() + str.slice(1)
+  }
+  else{
+    return str[0].toUpperCase() + str.slice(1).repeat(2)
+  }
+}
