002a71843069d2e46302de8154c206a81b10b20b
Obinna Ezeka <obinnae815@gmail.com>
2026-01-15 14:20:23 -0800

    OOP: Object Oriented Priacy

--- OopCodeWar.js
+++ OopCodeWar.js
@@ -9,15 +9,15 @@
 // experience	0
 
 // Solution 1: 
-// class Hero {
-// 	constructor(name = 'Hero') {
-//   	this.name = name;
-//     this.position = '00';
-//     this.health = 100;
-//     this.damage = 5;
-//     this.experience = 0;
-//   }
-// }
+class Hero {
+	constructor(name = 'Hero') {
+  	this.name = name;
+    this.position = '00';
+    this.health = 100;
+    this.damage = 5;
+    this.experience = 0;
+  }
+}
 
 // Explanation:
 // 1. We define a class named `Hero
@@ -28,13 +28,13 @@
 
 
 // Solution 2:
-// function Hero (name) {
-//   this.name = name || 'Hero';
-//   this.position = '00';
-//   this.health = 100;
-//   this.damage = 5;
-//   this.experience = 0;
-// }
+function Hero (name) {
+  this.name = name || 'Hero';
+  this.position = '00';
+  this.health = 100;
+  this.damage = 5;
+  this.experience = 0;
+}
 
 // Explanation:
 // 1. We define a function named `Hero` that acts as a constructor for creating hero objects.
@@ -42,3 +42,102 @@
 // 3. Inside the function, we use the `this` keyword to set the attributes of the hero object being created.
 // 4. Each attribute is initialized with the specified default values.
 // 5. When a new instance of the Hero function is created using the `new` keyword, it will have the defined attributes with their respective values.
+
+
+
+
+
+
+//Classy Extentions 
+// The goal of this kata is to train the basic OOP concepts of inheritance and method overriding.
+
+// You will be preloaded with the Animal class, so you should only edit the Cat class.
+
+// Task
+// Your task is to complete the Cat class which extends 
+// Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
+
+// The name attribute is accessible in the class with this.name.
+
+// Solution 1:
+class Cat extends Animal {
+  speak() {
+    return `${this.name} meows.`;
+  }
+}
+
+// Explanation:
+// 1. We define a class named `Cat` that extends the `Animal` class, inheriting its properties and methods.
+// 2. Inside the `Cat` class, we override the `speak` method to provide a specific implementation for cats.
+// 3. The `speak` method returns a string that combines the cat's name (accessed via `this.name`) with the word "meows."
+// 4. When the `speak` method is called on an instance of the `Cat` class, it will return the desired output format.
+
+// Solution 2:
+// function Cat(name) {
+//   Animal.call(this, name); // Call the parent constructor
+// }
+
+// Explanation:
+// 1. We define a constructor function named `Cat` that takes a `name` parameter.
+// 2. Inside the `Cat` constructor, we call the parent `Animal` constructor using `Animal.call(this, name)`, which initializes the inherited properties.
+// 3. This sets up the `Cat` object with the same properties as an `Animal`, including the `name` attribute.
+// 4. To override the `speak` method, we would typically add it to the prototype of `Cat`, but this part is not shown in this snippet.
+
+
+//OOP: OBject Oriented Piracy
+// Ahoy matey!
+
+// You are a leader of a small pirate crew. And you have a plan. 
+// With the help of OOP you wish to make a pretty efficient system to identify ships with heavy booty on board!
+
+// Unfortunately for you, people weigh a lot these days, so how do you know if a ship is full of gold and not people?
+
+// You begin with writing a generic Ship class / struct:
+
+// class Ship {
+//   constructor(draft, crew) {
+//     this.draft = draft
+//     this.crew = crew
+//   }
+// }
+// Every time your spies see a new ship enter the dock, they will create a new ship object based on their observations:
+
+// draft - an estimate of the ship's weight based on how low it is in the water
+// crew - the count of crew on board
+// const titanic = new Ship(15, 10);
+// Task
+// You have access to the ship "draft" and "crew". 
+// "Draft" is the total ship weight and "crew" is the number of humans on the ship.
+
+// Each crew member adds 1.5 units to the ship draft. 
+// If after removing the weight of the crew, the draft is still more than 20, 
+// then the ship is worth looting. Any ship weighing that much must have a lot of booty!
+
+// Add the method
+
+// isWorthIt
+// to decide if the ship is worthy to loot. For example:
+
+// titanic.isWorthIt() // return false
+// Good luck and may you find GOOOLD!
+
+// Solution 1:
+class Ship {
+  constructor(draft, crew) {
+    this.draft = draft;
+    this.crew = crew;
+  }
+
+  isWorthIt() {
+    return (this.draft - this.crew * 1.5) > 20;
+  }
+}
+
+// Explanation:
+// 1. We define a class named `Ship` with a constructor that takes `draft` and `crew` as parameters.
+// 2. Inside the constructor, we initialize the `draft` and `crew` attributes of the ship instance.
+// 3. We add a method named `isWorthIt` to the `Ship` class.
+// 4. The `isWorthIt` method calculates the effective draft by subtracting 
+// the weight contributed by the crew (1.5 units per crew member) from the total draft.
+// 5. It then checks if the resulting draft is greater than 20 and returns `true` if it is, 
+// indicating that the ship is worth looting; otherwise, it returns `false`.
