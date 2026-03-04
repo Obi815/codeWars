c48339e21ee4086945a5ce769e81119f2fb8e668
Obinna Ezeka <obinnae815@gmail.com>
2026-03-03 17:35:22 -0800

    Solution

--- 8Kyu/filterUnusedDigits.js
+++ 8Kyu/filterUnusedDigits.js
@@ -7,10 +7,22 @@
 // Note: the digits in the resulting string should be sorted.
 
 //Psuedo Code:
-function unusedDigits(){
+function unusedDigits(arr){
     // Filter through the digits given
-    
+        // Because they are varrying number of INT arguments use the rest parameter
+        // .join() them together to get numbers as a string
+
     // Compare the ten numbers [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] to the digits given
 
     // return digits that where not in the digits given 
+    
+}
+
+const unusedDigits = (...arr) => { // sets various args in array
+    
+    let digits = arr.join() // .join() adds comma inbetween numbers
+
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    .filter(number => !digits.includes(number)) //for each digit number, check if its absent from the digits string
+    .join('') // .join('') -> [0,1,2,3] to "0123"
 }
