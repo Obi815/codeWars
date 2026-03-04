41b76511a50aa74534e71c93c5b0b0342cb39f89
Obinna Ezeka <obinnae815@gmail.com>
2026-03-03 17:00:57 -0800

    Psuedo Code

---
+++ 8Kyu/filterUnusedDigits.js
@@ -0,0 +1,16 @@
+// Given a varying number of integer arguments, return the digits that are not present in any of them.
+
+// Example:
+
+// [12, 34, 56, 78]  =>  "09"
+// [2015, 8, 26]     =>  "3479"
+// Note: the digits in the resulting string should be sorted.
+
+//Psuedo Code:
+function unusedDigits(){
+    // Filter through the digits given
+    
+    // Compare the ten numbers [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] to the digits given
+
+    // return digits that where not in the digits given 
+}
