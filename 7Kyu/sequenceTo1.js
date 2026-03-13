453dd0d52d9f270b0223c1b7108c8fa8552baf20
Obinna Ezeka <obinnae815@gmail.com>
2026-03-12 18:09:03 -0700

    solution

--- 7Kyu/sequenceTo1.js
+++ 7Kyu/sequenceTo1.js
@@ -12,8 +12,24 @@
 // Crystal    -9999 < n < 9999
 // Ruby       -9999 < n < 9999
 
+// What is it asking me to do? 
+    // 1. Take given number and return an array starting with number 
+    //     given and all the numbers leading to number 1
+
 
 function seqToOne(n){
-  //your code here
-  
+    let sequence = []
+
+    // Compare the numbers to 1
+    while(n !== 1){
+        sequence.push(n) // Pushes the current number to sequence
+
+        if(n > 1){ // check whether to add or subtrack
+            n--
+        }else{
+            n++
+        }
+    }
+    sequence.push(1) // push 1 after running a while loop
+    return sequence
 }
