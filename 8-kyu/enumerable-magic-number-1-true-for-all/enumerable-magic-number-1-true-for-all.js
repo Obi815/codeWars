6a3ec9c32061cd7273dc1555780ad7e0fc03736c
Obinna Ezeka <obinnae815@gmail.com>
2026-09-16 14:00:23 -0700

    Update solution - CodeHub

--- 8-kyu/enumerable-magic-number-1-true-for-all/enumerable-magic-number-1-true-for-all.js
+++ 8-kyu/enumerable-magic-number-1-true-for-all/enumerable-magic-number-1-true-for-all.js
@@ -1,3 +1,16 @@
+// Params: Given an array of numbers and a unknown function
+// Return: Depending on the function check return true or false 
+​
+// Example:
+//   all((1, 2, 3, 4, 5), greater_than_9) -> false
+//   all((1, 2, 3, 4, 5), less_than_9)    -> True
+​
+// Pseudo: 
+//   Because we do not know the function we can just pass it in as a param
+//   We want every item in array to pass so we use the 'every' method to check it will return 
+//   true or false
+​
+​
 function all(arr, fun) {
-    return arr.every(fun) ? true : false
+    return arr.every(fun)
 }
